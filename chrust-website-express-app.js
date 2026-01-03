e1269531d008e02194e1ca129e30c829e7af7e29
Karol Konop <karol.konop@gmail.com>
2026-01-03 13:57:14 +0100

    update

--- chrust-website-express-app.js
+++ chrust-website-express-app.js
@@ -4,7 +4,7 @@ var express = require('express'),
   http = require('http'),
   httpServer = http.Server(app);
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname+'/public'));
 
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html');
